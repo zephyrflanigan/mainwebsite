c7fe9a7dda66887a25c5ceb02fb6801d0f2c5bb3
zephyrflanigan <zephyrflanigan28@gmail.com>
2026-06-11 23:15:59 -0400

    changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://zephyrflanigan.com',
+  site: 'https://zephyrflanigan.github.io',
+  base: '/websitedev',
   vite: {
     plugins: [tailwindcss()]
   }
